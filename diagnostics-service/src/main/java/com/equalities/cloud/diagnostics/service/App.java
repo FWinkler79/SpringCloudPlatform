99fd4c19fb59df36b5e55bf8f190f5393e292b70
FWinkler79 <52044081+FWinkler79@users.noreply.github.com>
2019-09-25 15:33:43 +0200

    Removed unnecessary Feign and Hystrix clients.

--- diagnostics-service/src/main/java/com/equalities/cloud/diagnostics/service/App.java
+++ diagnostics-service/src/main/java/com/equalities/cloud/diagnostics/service/App.java
@@ -2,14 +2,10 @@ package com.equalities.cloud.diagnostics.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
-@EnableCircuitBreaker
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);

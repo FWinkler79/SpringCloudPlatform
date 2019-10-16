ca17421e6066669ba95ea04290c15f01165b7924
FWinkler79 <52044081+FWinkler79@users.noreply.github.com>
2019-10-16 13:08:23 +0200

    Changes: - changed RSocket server's and client's dependency to Spring Boot Parent to 2.2.0.RC1 - Changed RSocket server's customization of lease handling after fix of Spring team. - Commented alternative lease handling for future reference. - Added a service gateway skeleton.

--- rsocket-server/src/main/java/com/equalities/cloud/rsocket/server/RsocketServerApplication.java
+++ rsocket-server/src/main/java/com/equalities/cloud/rsocket/server/RsocketServerApplication.java
@@ -2,47 +2,54 @@ package com.equalities.cloud.rsocket.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.rsocket.server.ServerRSocketFactoryProcessor;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 //@ComponentScan(basePackageClasses = {LeaseConfiguration.class}, includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = LeaseConfiguration.class))
 public class RsocketServerApplication {
 
-	public static void main(String[] args) {
-	  SpringApplication springApplication = new SpringApplication(RsocketServerApplication.class);
-    
-	  // Make sure we run reactive. 
-    // See: https://docs.spring.io/spring-boot/docs/2.2.0.M6/reference/html/spring-boot-features.html#boot-features-web-environment
-	  // springApplication.setWebApplicationType(WebApplicationType.REACTIVE); 
-    
-	  springApplication.run(args);	
-	}
-	
-// This would be the preferred way to customize lease handling and other properties of the
-// ServerRSocketFactory. However, this does not quite work yet. The Spring guys are only just 
-// getting this integrated into their SNAPSHOTs. 
-//
-// @Bean
-// public ServerRSocketFactoryCustomizer leaseCustomizer() {
-//   // Here, we return a ServerRSocketFactoryCustomizer bean to influence 
-//   // how the RSocket server is configured.
-//   //
-//   // A ServerRSocketFactory is defined by rsocket-java as an API that 
-//   // is used to create a server-side RSocket, using RSocketFactory.receive(). 
-//   // Among other things, it is used to configure leases to clients as shown in this sample:
-//   // https://github.com/rsocket/rsocket-java/blob/master/rsocket-examples/src/main/java/io/rsocket/examples/transport/tcp/lease/LeaseExample.java
-//   // 
-//   // On the client side, a similar class, ClientRSocketFactory, exists.
-//   // This can be customized using the RSocketRequester.Builder's .rsocketFactory() method.
-//   // See: https://docs.spring.io/spring/docs/5.2.0.RELEASE/spring-framework-reference/web-reactive.html#rsocket-requester-client-advanced
-//   
-//   // See: org.springframework.boot.autoconfigure.rsocket.RSocketServerAutoConfiguration
-//   return new LeaseCustomizer();
-// }
+  public static void main(String[] args) {
+    SpringApplication springApplication = new SpringApplication(RsocketServerApplication.class);
 
-// For TCP, you can use this to customize the ServerRSocketFactory.
+    // Make sure we run reactive.
+    // See:
+    // https://docs.spring.io/spring-boot/docs/2.2.0.M6/reference/html/spring-boot-features.html#boot-features-web-environment
+    // springApplication.setWebApplicationType(WebApplicationType.REACTIVE);
+
+    springApplication.run(args);
+  }
+
+  // Important auto-configurations:
+  // RSocketServerAutoConfiguration
+  // RSocketMessagingAutoConfiguration
+  // WebFluxServerAutoConfiguration
+  @Bean
+  public ServerRSocketFactoryProcessor leaseCustomizer() {
+    // Here, we return a ServerRSocketFactoryProcessor bean to influence
+    // how the RSocket server is configured.
+    //
+    // A ServerRSocketFactory is defined by rsocket-java as an API that
+    // is used to create a server-side RSocket, using RSocketFactory.receive().
+    // Among other things, it is used to configure leases to clients as shown in
+    // this sample:
+    // https://github.com/rsocket/rsocket-java/blob/master/rsocket-examples/src/main/java/io/rsocket/examples/transport/tcp/lease/LeaseExample.java
+    //
+    // On the client side, a similar class, ClientRSocketFactory, exists.
+    // This can be customized using the RSocketRequester.Builder's .rsocketFactory()
+    // method.
+    // See:
+    // https://docs.spring.io/spring/docs/5.2.0.RELEASE/spring-framework-reference/web-reactive.html#rsocket-requester-client-advanced
+
+    // See:
+    // org.springframework.boot.autoconfigure.rsocket.RSocketServerAutoConfiguration
+    return new LeaseCustomizingProcessor();
+  }
+
+// For TCP, you could also use this to customize the ServerRSocketFactory.
 //	
 //  @Bean
-//  RSocketServerFactory rSocketServerFactory(RSocketProperties properties, ReactorResourceFactory resourceFactory, ObjectProvider<ServerRSocketFactoryCustomizer> customizers) {
+//  RSocketServerFactory rSocketServerFactory(RSocketProperties properties, ReactorResourceFactory resourceFactory, ObjectProvider<ServerRSocketFactoryProcessor> processors) {
 //    NettyRSocketServerFactory factory = new NettyRSocketServerFactory();
 //    
 //    factory.setResourceFactory(resourceFactory);
@@ -52,12 +59,11 @@ public class RsocketServerApplication {
 //    map.from(properties.getServer().getAddress()).to(factory::setAddress);
 //    map.from(properties.getServer().getPort()).to(factory::setPort);
 //    
-//    Collection<ServerRSocketFactoryCustomizer> serverCustomizers = customizers.orderedStream().collect(Collectors.toList());
-//    serverCustomizers.add(new LeaseCustomizer());
+//    Collection<ServerRSocketFactoryProcessor> serverFactoryProcessors = processors.orderedStream().collect(Collectors.toList());
+//    serverFactoryProcessors.add(new LeaseCustomizingProcessor());
 //    
-//    factory.setServerCustomizers(serverCustomizers);
+//    factory.setServerProcessors(serverFactoryProcessors);
 //    return factory;
 //  }
-	
 
 }

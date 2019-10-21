19a4c1663500134568309e3965c849dde5553f59
FWinkler79 <52044081+FWinkler79@users.noreply.github.com>
2019-10-21 13:27:27 +0200

    Figuring out exceptions.

--- reservation-service-client/src/main/java/com/equalities/reservation/service/client/ReservationServiceClient.java
+++ reservation-service-client/src/main/java/com/equalities/reservation/service/client/ReservationServiceClient.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReservationServiceClientApplication {
+public class ReservationServiceClient {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ReservationServiceClientApplication.class, args);
+		SpringApplication.run(ReservationServiceClient.class, args);
 	}
 
 }
---
+++ reservation-service-client/src/test/java/com/equalities/reservation/service/client/ReservationServiceClientTests.java
@@ -0,0 +1,13 @@
+//package com.equalities.reservation.service.client;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ReservationServiceClientTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}

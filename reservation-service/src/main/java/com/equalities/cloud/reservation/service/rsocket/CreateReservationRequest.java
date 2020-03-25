123405af9a5a364e4399d00647a8eb2f7a43267f
FWinkler79 <52044081+FWinkler79@users.noreply.github.com>
2020-03-25 11:30:35 +0100

    Removed clutter.

--- reservation-service/src/main/java/com/equalities/cloud/reservation/service/rsocket/CreateReservationRequest.java
+++
@@ -1,8 +0,0 @@
-package com.equalities.cloud.reservation.service.rsocket;
-
-import lombok.Data;
-
-@Data
-public class CreateReservationRequest {
-  private String reservationName;
-}
--- scripts/removeSecret.sh
+++
@@ -1,3 +0,0 @@
-echo "Usage: $0 <key>"
-echo "Removing secret 'secret/$1'."
-docker exec -it -e VAULT_ADDR='http://0.0.0.0:8200' -e VAULT_TOKEN=root dev-vault vault kv metadata delete secret/$1
